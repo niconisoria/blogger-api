725e386ede7652cb0623ced5fd5c494d1c7811ee
Nicolás Nisoria <nicolas.nisoria@globallogic.com>
2020-10-31 03:10:12 -0300

    Fix error serializer

--- app/serializers/error_serializer.rb
+++ app/serializers/error_serializer.rb
@@ -16,35 +16,8 @@ class ErrorSerializer
     end
   end
 
-  def check_relationship_error(child)
-    child.errors.messages.map do |field, errors|
-      errors.map do |error_message|
-        {
-          source: { pointer: "/data/attributes/#{child.model_name.plural}[#{index}].#{field}" },
-          detail: error_message
-        }
-      end
-    end
-  end
-
-  def format_relationship_errors
-    errors = []
-    @model.class.reflect_on_all_associations.each do |relationship|
-      relationship = @model.send(relationship.name)
-      if relationship.respond_to?(:each)
-        relationship.each do |child|
-          errors << check_relationship_error(child)
-        end
-      else
-        errors << check_relationship_error(relationship)
-      end
-    end
-    errors
-  end
-
   def serializable_hash
     errors = format_common_errors
-    errors << format_relationship_errors
     { 'errors': errors.flatten }
   end
 end
